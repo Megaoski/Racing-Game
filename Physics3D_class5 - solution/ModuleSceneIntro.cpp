2514b58090f74dd1e1bfef1545dca32d3390f4a1
Megaoski <oscar_2596@hotmail.com>
2017-12-12 10:54:19 +0100

    First commit
    Adding core code

---
+++ Physics3D_class5 - solution/ModuleSceneIntro.cpp
@@ -0,0 +1,56 @@
+#include "Globals.h"
+#include "Application.h"
+#include "ModuleSceneIntro.h"
+#include "Primitive.h"
+#include "PhysBody3D.h"
+#include "ModulePhysics3D.h"
+
+ModuleSceneIntro::ModuleSceneIntro(Application* app, bool start_enabled) : Module(app, start_enabled)
+{
+}
+
+ModuleSceneIntro::~ModuleSceneIntro()
+{}
+
+// Load assets
+bool ModuleSceneIntro::Start()
+{
+	LOG("Loading Intro assets");
+	bool ret = true;
+
+	App->camera->Move(vec3(1.0f, 1.0f, 0.0f));
+	App->camera->LookAt(vec3(0, 0, 0));
+
+	return ret;
+}
+
+// Load assets
+bool ModuleSceneIntro::CleanUp()
+{
+	LOG("Unloading Intro scene");
+
+	return true;
+}
+
+// Update
+update_status ModuleSceneIntro::Update(float dt)
+{
+	Plane p(0, 1, 0, 0);
+	p.axis = true;
+	p.Render();
+
+	return UPDATE_CONTINUE;
+}
+
+void ModuleSceneIntro::OnCollision(PhysBody3D* body1, PhysBody3D* body2)
+{
+}
+
+void ModuleSceneIntro::CreateStraight(vec3 pos)
+{
+	Cube c1(3, 2, 3);
+	c1.SetPos(0, 0, 0);
+	App->physics->AddBody();
+	c1.Render();
+
+}
